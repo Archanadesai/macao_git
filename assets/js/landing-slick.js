f1ed73d9641b01ae7ef9711b298ffa4ac3b00cf4
januwebiots <janu.chahan@webiots.com>
2025-12-29 14:56:13 +0530

    landing page home section redesign

--- assets/js/landing-slick.js
+++ assets/js/landing-slick.js
@@ -1,64 +1,116 @@
-(function ($) {
-  $(".animate-slider").slick({
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 20000,
-    pauseOnHover: false,
-    cssEase: "linear",
-  });
+// (function ($) {
+//   $(".animate-slider").slick({
+//     slidesToShow: 1,
+//     autoplay: true,
+//     autoplaySpeed: 0,
+//     speed: 20000,
+//     pauseOnHover: false,
+//     cssEase: "linear",
+//   });
 
-  $(".testimonial-slider").slick({
+//   $(".testimonial-slider").slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//     autoplaySpeed: 800,
+//     dots: true,
+//     responsive: [
+//       {
+//         breakpoint: 1199,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           infinite: true,
+//           centerMode: true,
+//           centerPadding: "300px",
+//         },
+//       },
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           centerMode: true,
+//           centerPadding: "200px",
+//         },
+//       },
+//       {
+//         breakpoint: 900,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           centerMode: true,
+//           centerPadding: "100px",
+//         },
+//       },
+//       {
+//         breakpoint: 650,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           centerMode: true,
+//           centerPadding: "50px",
+//         },
+//       },
+//       {
+//         breakpoint: 575,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           centerMode: true,
+//           centerPadding: "0px",
+//         },
+//       },
+//     ],
+//   });
+// })(jQuery);
+(function ($) {
+  // $(document).ready(function(){
+  //   $('.your-class').slick({
+  //   });
+  // });
+  $(".hero-image").slick({
+    centerMode: false,
+    centerPadding: "60px",
+    autoplay: true,
+    autoplaySpeed: 2000,
     slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 800,
-    dots: true,
+    dots: false,
     responsive: [
       {
-        breakpoint: 1199,
+        breakpoint: 769,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          centerMode: true,
-          centerPadding: "300px",
+          arrows: false,
+          centerMode: false,
+          centerPadding: "40px",
+          slidesToShow: 3,
         },
       },
       {
-        breakpoint: 992,
+        breakpoint: 993,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: "200px",
+          arrows: false,
+          centerMode: false,
+          centerPadding: "40px",
+          slidesToShow: 3,
         },
       },
       {
-        breakpoint: 900,
+        breakpoint: 577,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: "100px",
+          arrows: false,
+          centerMode: false,
+          centerPadding: "40px",
+          slidesToShow: 2,
         },
       },
       {
-        breakpoint: 650,
+        breakpoint: 321,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: "50px",
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: "0px",
+          arrows: false,
+          centerMode: false,
+          centerPadding: "40px",
+          slidesToShow: 2,
         },
       },
     ],
